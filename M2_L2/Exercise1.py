ab7f0b1a086cdcf4f299a067d4717d588bae8e77
BrittBlack <brittneysblackmon@gmail.com>
2024-04-25 21:42:28 -0500

    exercise update

---
+++ M2_L2/Exercise1.py
@@ -0,0 +1,13 @@
+#Exercise 1: Traffic Light Simulator
+#
+#
+traffic_light = "black"
+
+if traffic_light == "red":
+    print("Stop!")
+elif traffic_light == "yellow":
+    print("Caution!")
+elif traffic_light == "green":
+    print("Go!")
+else:
+    print("Detour!")
