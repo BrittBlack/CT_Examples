ae16d5efc7f60db4011cf9a28cfb58bf7e309a00
BrittBlack <brittneysblackmon@gmail.com>
2024-04-27 07:50:12 -0500

    excercise update

---
+++ M2_L2/Exercise3.py
@@ -0,0 +1,16 @@
+#Exercise 3: Weather Suggestion
+#
+#
+#
+current_temperature = 85
+
+if current_temperature <= 0:
+    print("Put on your apple bottom jeans, boots with the fur, and maybe stay inside")
+elif current_temperature is > 25 and < 50:
+    print("You can get away with shorts, a sweater, and a parka.")
+elif current_temperature >== 50 and ==< 75:
+    print("Enjoy the weather, wear whatever you like")
+elif current_temperature >== 75 and ==< 100:
+    print("Just wear a swim suit out.")
+else:
+    print("Don't put on clothes, stay inside.")
